0b409563793fc78ea1e8955f4206c323630d7981
Yesidh <944@holbertonschool.com>
2021-03-01 09:59:47 -0500

    solving some problems with those files

---
+++ instagramclone/urls.py
@@ -0,0 +1,22 @@
+"""Modulo URL del instagramclone"""
+
+# Django
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+
+# local
+from instagramclone import views as local_views
+from posts import views as posts_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('hello-world/', local_views.hello_world),
+    path('sort/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>/', local_views.hi),
+
+    path('posts/', posts_views.list_posts)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
