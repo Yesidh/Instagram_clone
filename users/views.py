c94071243103ec2187e52f5a027eb3e3ba173687
Yesidh <944@holbertonschool.com>
2021-03-02 15:27:58 -0500

    [creating signup page and view]

--- users/views.py
+++ users/views.py
@@ -5,6 +5,14 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Exceptions
+from django.db.utils import IntegrityError
+
+
 def login_view(request):
     """Login view"""
     # import pdb; pdb.set_trace()
@@ -18,6 +26,30 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'invalid username or password'})
     return render(request, 'users/login.html')
+
+
+def signup_view(request):
+    """Users registration"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['passwd_confirmation']
+
+        if password != password_confirm:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+    return render(request, 'users/signup.html')
 
 
 @login_required
