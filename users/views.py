326aa4d022e23323a1b217b6ed9fa3e68c220730
Yesidh <944@holbertonschool.com>
2021-03-03 10:56:36 -0500

    [using form for update profile view]

--- users/views.py
+++ users/views.py
@@ -12,10 +12,38 @@ from users.models import Profile
 # Exceptions
 from django.db.utils import IntegrityError
 
+# Forms
+from users.forms import ProfileForm
+
 
 def update_profile(request):
     """Updates a users profile view."""
-    return render(request, 'users/update_profile.html')
+
+    profile = request.user.profile
+    form = ProfileForm()
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+        else:
+            form = ProfileForm()
+    return render(request=request,
+                  template_name='users/update_profile.html',
+                  context={
+                      'profile': profile,
+                      'user': request.user,
+                      'form': form
+                  })
+
 
 def login_view(request):
     """Login view"""
